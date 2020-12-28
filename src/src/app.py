fd3f15ab90d96756027e3cc485dd6825d6337e6b
Patrycja Szczepaniak <szczepaniak7@gmail.com>
2020-12-28 18:28:19 +0100

    Added function to upload file

--- src/src/app.py
+++ src/src/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 
 app = FastAPI()
@@ -7,6 +7,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-# @app.post("/files")
-# def read_item(q: Optional[str] = None):
-#     return {"file_id": file_id, "q": q}
+@app.post("/files/")
+async def create_upload_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
