0970bf440ea64f853293409327838683d7c3d104
Łukasz Leszczyński <luke.leszczynski@gmail.com>
2021-01-03 14:28:48 +0100

    Indexer request

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI, File, UploadFile
+import requests
 
 
 app = FastAPI()
+
+class IndexerData(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: Optional[str] = None
 
 @app.get("/")
 def read_root():
@@ -11,7 +18,7 @@ def read_root():
 def read_indexer(param: str):
     url = "https://ocr-a.search.windows.net/indexes/azureblob-index-2/docs?api-version=2020-06-30&search="
     url = url + param
-    response = RedirectResponse(url=url)
+    response = requests.get(url)
     return {"response": response}
 
 @app.post("/files/")
