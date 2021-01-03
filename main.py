9521d2820830fcab4c6ed719e1d6ddc19e8de9e7
Łukasz Leszczyński <luca2014@gmail.com>
2021-01-03 21:45:30 +0100

    Add Value model

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ app = FastAPI()
 class Index(BaseModel):
     context: str
     value: Optional[str] = None
+        
+class Value(BaseModel):
+    score: float
+    content: str
+    metadata_storage_content_type: str
+    metadata_storage_path: str
+    language: str
+    merged_content: str
+    text: str
+    layoutText: str
 
 @app.get("/")
 def read_root():
@@ -21,11 +31,16 @@ def read_indexer(param: str):
     url = "https://ocr-a.search.windows.net/indexes/azureblob-index-2/docs?api-version=2020-06-30&api-key=" + api_key + "&search=" + param
     response = requests.get(url).json()
     index = create_index(response)
-    return {"value": index['value']}
+    value = create_value(index['value'])
+    return {"value": value['text']}
 
 @app.post("/indexes/")
 def create_index(index: Index):
     return index
+
+@app.post("/values/")
+def create_value(value: Value):
+    return value
 
 @app.post("/files/")
 async def create_upload_file(file: UploadFile = File(...)):
