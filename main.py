72bce802096d2cd115864818f26dc31334d6f17e
Łukasz Leszczyński <luke.leszczynski@gmail.com>
2021-01-03 13:51:58 +0100

    Create indexer url

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/search/{param}")
+def read_indexer(param: str):
+    url = "https://ocr-a.search.windows.net/indexes/azureblob-index-2/docs?api-version=2020-06-30&search="
+    url = url + param
+    #response = RedirectResponse(url=url)
+    return {"response": url}
+
 @app.post("/files/")
 async def create_upload_file(file: UploadFile = File(...)):
     return {"filename": file.filename}
