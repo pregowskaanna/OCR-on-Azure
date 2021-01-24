2d3f9080f321255cbe562c38f09a1dbfd199611c
pregowsa <pregowskaanna@gmail.com>
2021-01-24 12:11:37 +0100

    hotfix n+7

--- main.py
+++ main.py
@@ -43,25 +43,25 @@ def create_index(index: Index):
 def create_value(value: Value):
     return value
 
-@app.post("/files/")
-async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+# @app.post("/files/")
+# async def create_upload_file(file: UploadFile = File(...)):
+#     return {"filename": file.filename}
 
-@app.put("/files/create/")
-async def create_file(request: Request, file: UploadFile = File(...)):
-    content_length = request.headers['content-length']
-    headers={"x-ms-type":file.file,
-    "x-ms-content-length": content_length}
-    filename = file.filename
+# @app.put("/files/create/")
+# async def create_file(request: Request, file: UploadFile = File(...)):
+#     content_length = request.headers['content-length']
+#     headers={"x-ms-type":file.file,
+#     "x-ms-content-length": content_length}
+#     filename = file.filename
 
-    uri_create = f"https://bfkhabfkjwhfohfejwgfkg.file.core.windows.net/personal/data/{filename}?sv=2019-12-12&ss=bf&srt=co&sp=rwdlacx&se=2021-01-22T03:59:59Z&st=2021-01-21T19:59:59Z&spr=https&sig=8Gw3DdkeqrMecXJBmUgYAXPslIpLGApEKronGquesh4%3D"
-    #try:
-    response = requests.put(uri_create, headers=headers)
-    #except requests.exceptions.HTTPError as e:
+#     uri_create = f"https://bfkhabfkjwhfohfejwgfkg.file.core.windows.net/personal/data/{filename}?sv=2019-12-12&ss=bf&srt=co&sp=rwdlacx&se=2021-01-22T03:59:59Z&st=2021-01-21T19:59:59Z&spr=https&sig=8Gw3DdkeqrMecXJBmUgYAXPslIpLGApEKronGquesh4%3D"
+#     #try:
+#     response = requests.put(uri_create, headers=headers)
+#     #except requests.exceptions.HTTPError as e:
 
-    # response = upload_file(file)
-    return response.raw
-
+#     # response = upload_file(file)
+#     return response.raw
+@app.put("/files/create/", status_code=200)
 async def upload_file(file: UploadFile = File(...)):
     filename = file.filename
     
@@ -69,17 +69,18 @@ async def upload_file(file: UploadFile = File(...)):
     "x-ms-content-length":len(bytes(file))}
     uri_upload = f"https://bfkhabfkjwhfohfejwgfkg.file.core.windows.net/personal/data/{filename}?comp=range&?sv=2019-12-12&ss=bf&srt=co&sp=rwdlacx&se=2021-01-22T03:59:59Z&st=2021-01-21T19:59:59Z&spr=https&sig=8Gw3DdkeqrMecXJBmUgYAXPslIpLGApEKronGquesh4%3D"
     response = requests.put(uri_upload, headers=headers)
-    return response
+    return {}
+    # return response
 
 
-@app.post("/files/uploadtest/")
-async def create_file2(
-    file: UploadFile = File(...)
-):
+# @app.post("/files/uploadtest/")
+# async def create_file2(
+#     file: UploadFile = File(...)
+# ):
 
-    filename = file.filename
+#     filename = file.filename
 
-    uri_create = f"https://bfkhabfkjwhfohfejwgfkg.file.core.windows.net/personal/data/{filename}?sv=2019-12-12&ss=bf&srt=co&sp=rwdlacx&se=2021-01-22T03:59:59Z&st=2021-01-21T19:59:59Z&spr=https&sig=8Gw3DdkeqrMecXJBmUgYAXPslIpLGApEKronGquesh4%3D"
-    response = requests.post(uri_create)
+#     uri_create = f"https://bfkhabfkjwhfohfejwgfkg.file.core.windows.net/personal/data/{filename}?sv=2019-12-12&ss=bf&srt=co&sp=rwdlacx&se=2021-01-22T03:59:59Z&st=2021-01-21T19:59:59Z&spr=https&sig=8Gw3DdkeqrMecXJBmUgYAXPslIpLGApEKronGquesh4%3D"
+#     response = requests.post(uri_create)
 
-    return response
+#     return response
